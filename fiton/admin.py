dd018899a161346a0757f9bc4a3001d703a76df6
mungwanwoo <ansrhksdn999@gmail.com>
2024-12-16 11:37:44 +0900

    (ft-base)base.html 작성

--- fiton/admin.py
+++ fiton/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+# fiton 앱의 모든 모델 자동 등록
+fiton_models = apps.get_app_config('fiton').get_models()
+for model in fiton_models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
